6a674a0f7efd1761cf50c63dc2c99d23590e3da5
YahyaShareef <yahyashareef48@gmail.com>
2024-01-16 20:52:06 +0530

    icon fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <link rel="shortcut icon" href="/images/LOGO.webp" type="image/x-icon" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
