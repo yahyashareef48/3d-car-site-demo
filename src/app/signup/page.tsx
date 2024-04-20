086505b31d194e628ca322b750e48500272dc1e2
YahyaShareef <yahyashareef48@gmail.com>
2024-04-20 20:01:31 +0530

    sign up form done

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Navbar } from "@/components";
 import { useState } from "react";
 
 function Login() {
@@ -16,7 +17,7 @@ function Login() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ email, password, name }),
+        body: JSON.stringify({ email, name }),
       }).catch((error) => console.error("Error:", error));
     } catch (error) {
       console.error("Error during login: ", error);
@@ -24,32 +25,56 @@ function Login() {
   };
 
   return (
-    <form className="text-black" action="POST" onSubmit={handleSubmit}>
-      <input
-        type="email"
-        name="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        name="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <input
-        type="text"
-        name="name"
-        placeholder="Type Your Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <button className="text-white" type="submit">
-        Submit
-      </button>
-    </form>
+    <>
+      <Navbar />
+
+      <section className="min-h-screen min-w-full flex justify-center items-center p-4">
+        <div className="w-full lg:max-w-xl p-6 space-y-8 sm:p-8 bg-[#232323] rounded-lg shadow-xl">
+          <h2 className="text-2xl font-bold text-gray-900 dark:text-white">
+            Sign up to 3D Car Designer
+          </h2>
+          <form className="mt-8 space-y-6" action="POST" onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="email" className="block mb-2 text-sm font-medium text-white">
+                Your email
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="border text-sm rounded-lg block w-full p-2.5 bg-[#232323] border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                placeholder="Your email"
+                required
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="name"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Your full name
+              </label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Your full name"
+                className="border text-sm rounded-lg block w-full p-2.5 bg-[#232323] border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                required
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 sm:w-auto dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Sign up
+            </button>
+          </form>
+        </div>
+      </section>
+    </>
   );
 }
 
